6f28d6a91aafd3230847ff45de88991407cb0c7d
Eliut González <reliutg@gmail.com>
2021-08-31 00:41:52 +0200

    chore: cli support

---
+++ src/cli.js
@@ -0,0 +1,46 @@
+const arg = require("arg");
+const { getRepoContributors, generateSvg, downloadSvg } = require(".");
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      "--owner": String,
+      "--name": String,
+      "--token": String,
+      "-o": "--owner",
+      "-n": "--name",
+      "-t": "--token",
+    },
+    {
+      argv: rawArgs.slice(2),
+    }
+  );
+  return {
+    repoOwner: args["--owner"],
+    repoName: args["--name"],
+    token: args["--token"],
+  };
+}
+
+function cli(args) {
+  const parsedArgs = parseArgumentsIntoOptions(args);
+  // check if have repoOwner and repoName
+  if (!parsedArgs.repoOwner || !parsedArgs.repoName) {
+    console.log("Please provide a repo owner and repo name");
+    return;
+  }
+  getRepoContributors(
+    parsedArgs.repoOwner,
+    parsedArgs.repoName,
+    parsedArgs.token
+  ).then((contributors) => {
+    if (contributors.length === 0) {
+      console.log("No contributors found");
+      return;
+    }
+    const svg = generateSvg(contributors);
+    downloadSvg(svg);
+  });
+}
+
+module.exports = { cli };
---
+++ src/index.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const { Octokit } = require("octokit");
+const octokit = new Octokit();
+
+function getRepoContributors(owner, repo) {
+  const data = octokit
+    .request("GET /repos/{owner}/{repo}/contributors  ", {
+      owner,
+      repo,
+    })
+    .then((res) => {
+      const contributors = res.data.map((user) => {
+        return {
+          name: user.login,
+          avatar_url: user.avatar_url,
+        };
+      });
+      if (!Array.isArray(contributors)) {
+        console.log("Error: ", contributors.message);
+        return [];
+      }
+      return contributors;
+    })
+    .catch((err) => {
+      console.log("Error: ", err.message);
+      return [];
+    });
+  return data;
+}
+
+function generateSvg(contributors) {
+  let svg = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" width="812" height="472">`;
+
+  // for round corner
+  svg += `<clipPath id="clip" clipPathUnits="objectBoundingBox">
+              <rect ry="0.5" width="1" height="1" fill="black" />
+            </clipPath>
+            `;
+
+  let distanceX = 68;
+  let distanceY = 0;
+  let index = 0;
+  let line = 0;
+
+  contributors.forEach((user) => {
+    let nextX = index * distanceX;
+    if (nextX / 11 === 68) {
+      line++;
+      index = -1;
+      // create new line
+      if (line >= 2) {
+        distanceY += 68;
+      }
+    }
+    svg += `
+          <svg x="${nextX}" y="${distanceY}">
+            <a href="https://github.com/${user.name}" target="_blank">
+                <title>${user.name}</title>
+              <image href="${user.avatar_url}" height="64" width="64"  clip-path="url(#clip)" />
+            </a>
+          </svg>
+            `;
+    index++;
+  });
+  svg += `</svg>`;
+
+  return svg;
+}
+
+function downloadSvg(svg) {
+  const minified = svg.replace(/\s+/g, " ");
+  fs.writeFile("contributors.svg", minified, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+}
+
+module.exports = { getRepoContributors, generateSvg, downloadSvg };
